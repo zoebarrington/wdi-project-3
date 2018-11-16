2bd0777eed84aefcad007e85e25a18b04dfe60ce
theodorabirch <theodorabirch@gmail.com>
2018-11-16 15:03:41 +0000

    merge

---
+++ models/movie.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+const movieSchema = mongoose.Schema({
+  name: String,
+  yearReleased: Number,
+  movieLength: String,
+  image: String,
+  writers: String,
+  director: String,
+  actors: [String],
+  synopsis: String,
+  locationFilmed: [String],
+  genres: [String],
+  reviews: {
+    title: String,
+    rating: Number,
+    content: String,
+    createdBy: {
+      type: mongoose.Schema.ObjectId, ref: 'User'
+    },
+    likedBy: [{
+      type: mongoose.Schema.ObjectId, ref: 'User'
+    }],
+    date: String
+  },
+  averageRatings: Number,
+  createdBy: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'User'
+  }
+});
+
+const movieModel = mongoose.model('Movie', movieSchema);
+module.exports = movieModel;
