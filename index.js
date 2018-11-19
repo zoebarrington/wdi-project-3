c21fb41335cfca0ba6593c8f084a4f9153132faf
Zoe Barrington <zlfbarrington@gmail.com>
2018-11-19 15:33:54 +0000

    added static

--- index.js
+++ index.js
@@ -10,7 +10,9 @@ const app = express();
 mongoose.connect(env.dbUri);
 app.use(bodyParser.json());
 app.use(morgan('dev'));
+app.use(express.static(`${__dirname}/public`));
 app.use('/api', router);
+
 
 app.listen(port, () => console.log(`Express is running on port ${port}`));
 
