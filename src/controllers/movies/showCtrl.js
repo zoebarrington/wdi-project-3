a32e08eaf8e6a45b6059077d29ae2166ec070446
Zoe Barrington <zlfbarrington@gmail.com>
2018-11-19 16:37:41 +0000

    deleting comments done

--- src/controllers/movies/showCtrl.js
+++ src/controllers/movies/showCtrl.js
@@ -1,54 +1,30 @@
-// function showCtrl($scope, $http, $state) {
-//   $http({
-//     method: 'GET',
-//     url: `/api/movies/${$state.params.id}`
-//   }).then(result => {
-//     $scope.movie = result.data;
-//   });
-//   $scope.createReview = function() {
-//     $scope.review.createdBy = $scope.userId;
-//     $http({
-//       method: 'POST',
-//       url: `/api/movies/${$state.params.id}/reviews`,
-//       data: $scope.review
-//     }).then(result => $scope.movie = result.data);
-//   };
-//   $scope.handleDelete = function () {
-//     $http({
-//       method: 'DELETE',
-//       url: `/api/movies/${$state.params.id}`
-//     }).then(() => $state.go('movieIndex'));
-//   };
-// }
 
-function showCtrl($state, $scope, $http) {
-  $scope.review = {};
+function showCtrl($scope, $http, $state) {
   $http({
     method: 'GET',
-    url: `api/movies/${$state.params.id}`
+    url: `/api/movies/${$state.params.id}`
   }).then(result => {
     $scope.movie = result.data;
   });
 
   $scope.createReview = function() {
+    console.log($scope.review);
+    $scope.review.createdBy = $scope.userId;
     $http({
       method: 'POST',
-      url: `/api/movies/${$state.params.id}/comments`,
+      url: `/api/movies/${$state.params.id}/reviews`,
       data: $scope.review
-    }).then(result => {
-      $scope.movie = result.data;
-      $scope.review.content = null;
-    });
+    }).then(result => $scope.movie = result.data);
   };
   $scope.deleteme = function() {
     console.log($scope.review);
   };
 
-  $scope.deleteComment = function(review) {
+  $scope.deleteReview = function(review) {
     $http({
       method: 'DELETE',
       url: `/api/movies/${$state.params.id}/reviews/${review._id}`
-    }).then(result => $scope.movie= result.data);
+    }).then(result => $scope.movie = result.data);
   };
 
   $scope.handleDelete = function() {
