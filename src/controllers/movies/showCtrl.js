353e305f2deb61592445a046af0b88e83ec945f9
Zoe Barrington <zlfbarrington@gmail.com>
2018-11-19 15:31:31 +0000

    partially complete show routes

--- src/controllers/movies/showCtrl.js
+++ src/controllers/movies/showCtrl.js
@@ -1,24 +1,59 @@
-function showCtrl($scope, $http, $state) {
+// function showCtrl($scope, $http, $state) {
+//   $http({
+//     method: 'GET',
+//     url: `/api/movies/${$state.params.id}`
+//   }).then(result => {
+//     $scope.movie = result.data;
+//   });
+//   $scope.createReview = function() {
+//     $scope.review.createdBy = $scope.userId;
+//     $http({
+//       method: 'POST',
+//       url: `/api/movies/${$state.params.id}/reviews`,
+//       data: $scope.review
+//     }).then(result => $scope.movie = result.data);
+//   };
+//   $scope.handleDelete = function () {
+//     $http({
+//       method: 'DELETE',
+//       url: `/api/movies/${$state.params.id}`
+//     }).then(() => $state.go('movieIndex'));
+//   };
+// }
+
+function showCtrl($state, $scope, $http) {
+  $scope.review = {};
   $http({
     method: 'GET',
-    url: `/api/movies/${$state.params.id}`
+    url: `api/movies/${$state.params.id}`
   }).then(result => {
     $scope.movie = result.data;
   });
+
   $scope.createReview = function() {
-    $scope.review.createdBy = $scope.userId;
     $http({
       method: 'POST',
-      url: `/api/movies/${$state.params.id}/reviews`,
+      url: `/api/movies/${$state.params.id}/comments`,
       data: $scope.review
-    }).then(result => $scope.movie = result.data);
+    }).then(result => {
+      $scope.movie = result.data;
+      $scope.review.content = null;
+    });
   };
-  $scope.handleDelete = function () {
+
+  $scope.deleteComment = function(review) {
     $http({
       method: 'DELETE',
-      url: `/api/movies/${$state.params.id}`
-    }).then(() => $state.go('movieIndex'));
+      url: `/api/movies/${$state.params.id}/reviews/${review._id}`
+    }).then(result => $scope.movie= result.data);
   };
+
+  $scope.handleDelete = function() {
+    $http({
+      method: 'DELETE',
+      url: `/api/movies/${$scope.movie._id}`
+    }).then(() => $state.go('movieIndex'));
+  }
 }
 
 export default showCtrl;
