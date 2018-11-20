aaba7029ea7ee187824fcb864e3c2ba9fa71a011
Fred-Bell <fwgbell@gmail.com>
2018-11-20 13:50:49 +0000

    added updating to reviews

--- src/controllers/movies/showCtrl.js
+++ src/controllers/movies/showCtrl.js
@@ -30,6 +30,30 @@ function showCtrl($scope, $http, $state) {
       url: `/api/movies/${$scope.movie._id}`
     }).then(() => $state.go('movieIndex'));
   };
+
+  $scope.notReviewed = function(){
+    for(let i = 0; i < $scope.movie.reviews.length; i++){
+      if($scope.movie.reviews[i].createdBy._id === $scope.userId){
+        return false;
+      }
+    }
+    return true;
+  };
+
+  $scope.editReview = function(review){
+    $scope.yourReview = review;
+    $scope.editing = true;
+  };
+
+  $scope.updateReview = function(){
+    $scope.editing = false;
+    $http({
+      method: 'PUT',
+      url: `/api/movies/${$state.params.id}/reviews/${$scope.yourReview._id}`,
+      data: $scope.yourReview
+    }).then(() => $state.go('movieShow', { id: $state.params.id }));
+  };
+
 }
 
 export default showCtrl;
