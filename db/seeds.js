56bf6ee8e70b440910006c0c0bc81137d4194cd3
Zoe Barrington <zlfbarrington@gmail.com>
2018-11-23 10:01:01 +0000

    seed

--- db/seeds.js
+++ db/seeds.js
@@ -168,9 +168,9 @@ const movieData = [
     locationFilmed: ['Martha/s Vineyard, USA'],
     genres: ['Thriller'],
     reviews: [{
-      title: 'Sensible Title',
-      rating: 2,
-      content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      title: 'Compelling',
+      rating: 4,
+      content: 'Compelling, well-crafted storytelling and a judicious sense of terror ensure Steven Spielbergs Jaws has remained a benchmark in the art of delivering modern blockbuster thrills.',
       createdBy: userIds [1]
     }]
   }, {
